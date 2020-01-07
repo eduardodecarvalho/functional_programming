5e9434c09f16c9b9b65aea9230b70640f25210cb
eduardo_decarvalho <eduardobcarvalho@outlook.com>
2020-01-06 22:34:36 -0300

    reading a csv file and making some interactions

---
+++ src/main/java/com/eduardo/study/service/EmployeeService.java
@@ -0,0 +1,62 @@
+package com.eduardo.study.service;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import com.eduardo.study.model.Employee;
+
+public class EmployeeService {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			List<Employee> list = new ArrayList<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
+			}
+
+			System.out.print("Enter salary: ");
+			double salary = sc.nextDouble();
+			
+			List<String> emails = list.stream()
+					.filter(x -> x.getSalary() > salary)
+					.map(Employee::getEmail)
+					.sorted()
+					.collect(Collectors.toList());
+
+			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary) + ":");
+			emails.forEach(System.out::println);
+
+			System.out.print("Letter for filter salary: ");
+			Character letter = sc.next().charAt(0);
+
+			double sum = list.stream()
+					.filter(x -> x.getName().charAt(0) == letter)
+					.map(Employee::getSalary)
+					.reduce(0.0, (x, y) -> x + y);
+
+			System.out.println("Sum of salary from people whose name starts with " + letter + ": " + String.format("%.2f", sum));
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
+	}
+}
